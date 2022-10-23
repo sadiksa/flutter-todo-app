f363a2ebc182da17ffced4a3317fd115c92f724d
sadiksahin <sadiksahin95@gmail.com>
2022-10-23 15:17:06 +0300

    icon

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-const Color tdRed = Color.fromARGB(255, 255, 172, 172);
-const Color tdBlue = Color.fromARGB(255, 0, 255, 250);
+const Color tdRed = Color.fromARGB(255, 238, 0, 255);
+const Color tdBlue = Color.fromARGB(255, 68, 255, 0);
 
 const Color tdBlack = Color(0xFF3A3A3A);
 const Color tdGrey = Color(0xFF717171);
